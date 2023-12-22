4794898c23f246c46a31f6b92e8ad0c5b449f47e
tamanchichan <tamanchichan@gmail.com>
2023-12-22 13:09:17 -0600

    'Cart' class created

---
+++ fast-food/Models/Cart.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fast_food.Models
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+
+        public List<Item>? Items { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "SubTotal can not be less than zero")]
+        public decimal? SubTotal
+        {
+            get
+            {
+                return Math.Round(Items.Sum(i => i.Price), 2); // think about 'quantity' later
+            }
+        }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Total can not be less than zero")]
+        public decimal? Total
+        {
+            get
+            {
+                return Math.Round((decimal)SubTotal * 1.12m, 2); // '1.12m' represents the tax
+            }
+        }
+    }
+}
