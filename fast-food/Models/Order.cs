e7b45a5eb94fc4cb8617fdaa93f35e3d3ff3b11f
tamanchichan <tamanchichan@gmail.com>
2023-12-22 13:14:39 -0600

    'Order' class created

---
+++ fast-food/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fast_food.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+
+        public List<Item> Items { get; set; }
+
+        [Range(0, Double.MaxValue, ErrorMessage = "Total can not be less than zero.")]
+        public decimal Total { get; set; }
+    }
+}
