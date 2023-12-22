680da4f6e12cd2c798ab2f6e1c6f1bcb41a149cb
tamanchichan <tamanchichan@gmail.com>
2023-12-22 13:49:20 -0600

    Initial migration added, updated database, updated connection (to Azure)

--- fast-food/Areas/Identity/Data/FastFoodDb.cs
+++ fast-food/Areas/Identity/Data/FastFoodDb.cs
@@ -1,4 +1,5 @@
 ﻿using fast_food.Areas.Identity.Data;
+using fast_food.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -19,4 +20,9 @@ public class FastFoodDb : IdentityDbContext<FastFoodUser>
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
     }
+
+    public DbSet<Item> Item { get; set; } = default!;
+    public DbSet<Cart> Cart { get; set; } = default!;
+    public DbSet<CartItem> CartItems { get; set; } = default!;
+    public DbSet<Order> Order { get; set; } = default!;
 }
