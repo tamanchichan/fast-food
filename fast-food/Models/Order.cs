a0f24c2fee6ffe4d1d37aeff968db944f4d22bad
tamanchichan <tamanchichan@gmail.com>
2023-12-22 13:32:24 -0600

    Following the same properties as 'Cart'

--- fast-food/Models/Order.cs
+++ fast-food/Models/Order.cs
@@ -6,9 +6,24 @@ namespace fast_food.Models
     {
         public Guid Id { get; set; }
 
-        public List<Item> Items { get; set; }
+        public HashSet<CartItem>? CartItems { get; set; }
 
-        [Range(0, Double.MaxValue, ErrorMessage = "Total can not be less than zero.")]
-        public decimal Total { get; set; }
+        [Range(0, double.MaxValue, ErrorMessage = "SubTotal can not be less than zero")]
+        public decimal SubTotal
+        {
+            get
+            {
+                return Math.Round(CartItems.Sum(ci => ci.Item.Price * ci.Quantity), 2);
+            }
+        }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Total can not be less than zero")]
+        public decimal Total
+        {
+            get
+            {
+                return Math.Round((decimal)SubTotal * 1.12m, 2); // '1.12m' represents the tax
+            }
+        }
     }
 }
