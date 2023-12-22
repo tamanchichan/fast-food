ca78dff4db0ed68560cb047ca24a31407786412a
tamanchichan <tamanchichan@gmail.com>
2023-12-22 13:16:46 -0600

    'CartItem' class created

---
+++ fast-food/Models/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fast_food.Models
+{
+    public class CartItem
+    {
+        public Guid Id { get; set; }
+
+        public Guid ItemId { get; set; }
+        public Item Item { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity can not be less than zero.")]
+        public int Quantity { get; set; }
+    }
+}
