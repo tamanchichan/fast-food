63519daa4f2e1afb44b1860b4880ac7901a1e64d
tamanchichan <tamanchichan@gmail.com>
2024-06-05 10:52:25 -0500

    'Cart' and 'CartItem' classes created

---
+++ fast-food/Models/Cart.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fast_food.Models
+{
+    public class Cart
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public HashSet<CartItem> Items { get; set;} = new HashSet<CartItem>();
+
+        // Calculates the subtotal by summing up the prices of all items in the cart
+        public decimal SubTotal
+        {
+            get { return Items.Sum(i => i.Price); }
+        }
+
+        // Calculates the total by adding a 12% tax to the subtotal
+        public decimal Total
+        {
+            get { return SubTotal * 1.12m; }
+        }
+    }
+}
---
+++ fast-food/Models/CartItem.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace fast_food.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "'Code' has to be at least 1 character length")]
+        [DisplayName("Code")]
+        public string Code { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "'Name' has to be at least 3 character length")]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Price")]
+        public decimal Price { get; set; }
+
+        [ForeignKey("CartId")]
+        public Guid CartId { get; set; }
+
+        public Cart Cart { get; set; }
+    }
+}
