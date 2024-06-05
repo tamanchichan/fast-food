093579e0890b1b00bd0b6e109654e4d0ed6adf71
tamanchichan <tamanchichan@gmail.com>
2024-06-05 10:57:20 -0500

    'Order' and 'OrderItem' class created

---
+++ fast-food/Models/Order.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace fast_food.Models
+{
+    public class Order
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [DisplayName("Date of Creation")]
+        public DateTime DateOfCreation { get; set; } = DateTime.Now;
+
+        public HashSet<OrderItem> Items { get; set; } = new HashSet<OrderItem>();
+
+        // Calculates the subtotal by summing up the prices of all items in the cart
+        public decimal SubTotal
+        {
+            get { return Items.Sum(i => i.Price); }
+        }
+
+        // Calculates the total by adding a 12% tax to the subtotal
+        public decimal Total
+        {
+            get { return SubTotal * 1.12m; }
+        }
+    }
+}
---
+++ fast-food/Models/OrderItem.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace fast_food.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "'Code' has to be at least 1 character length")]
+        [DisplayName("Code")]
+        public string Code { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "'Name' has to be at least 3 character length")]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Price")]
+        public decimal Price { get; set; }
+
+        [ForeignKey("OrderId")]
+        public Guid OrderId { get; set; }
+
+        public Order Order { get; set; }
+    }
+}
