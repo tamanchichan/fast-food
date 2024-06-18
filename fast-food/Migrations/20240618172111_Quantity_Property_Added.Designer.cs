2fa2c3f0b86f9daa5dc384c1556d9e92e06a6836
tamanchichan <tamanchichan@gmail.com>
2024-06-18 12:21:51 -0500

    Migration added to implement 'Quantity' property

--- fast-food/Migrations/20240618172111_Quantity_Property_Added.Designer.cs
+++ fast-food/Migrations/20240618172111_Quantity_Property_Added.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using fast_food.Data;
 
@@ -11,9 +12,10 @@ using fast_food.Data;
 namespace fast_food.Migrations
 {
     [DbContext(typeof(FastFoodDbContext))]
-    partial class FastFoodDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240618172111_Quantity_Property_Added")]
+    partial class Quantity_Property_Added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ fast-food/Migrations/20240618172111_Quantity_Property_Added.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fast_food.Migrations
+{
+    public partial class Quantity_Property_Added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "OrderItem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "CartItem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "CartItem");
+        }
+    }
+}
